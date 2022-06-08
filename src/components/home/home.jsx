87ed0f530d476b984c5bb960cd503eeff4a40216
Mayra Rivas Lara <mayrulara@hotmail.com>
2022-06-08 12:21:54 +0800

    responssiveness working

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,38 +1,52 @@
 import React from 'react';
 import '../home/home.css';
-import Me from '../../assets/MeBW.jpg';
+import Photo from '../../assets/ph1.jpg';
 
 export default function Home() {
   return (
     
     <div className='home'>
-      <div className='home-left'>
-        <div className="home-left-wrapper">
+      <div className='container'>
+        <div className="row align-items-center my-5">
+          <div className='col-lg-6'>
+            <img 
+            src={Photo}
+            id= 'home-photo'
+            alt='homephoto'
+            />
           <h2 className='home-greet'>Hello, My name is</h2>
           <h1 className='home-name'>Mayra Rivas Lara</h1>
           <div className="home-title">
+
+
             <div className="home-title-wrapper">
-              <div className="home-title-item">Full-Stack Web Developer</div>
-              <div className="home-title-item">UI/UX Designer</div>
-              <div className="home-title-item">Photographer</div>
-              <div className="home-title-item">Writer</div>
-              <div className="home-title-item">Contents Creator</div>
-              <div className="home-title-item">Mother</div>
+              <div className="home-title-item">Junior Full-Stack Web Developer</div>
+              <div className="home-title-item">UI/UX Enthusiast</div>
+              <div className="home-title-item">Great Communicator</div>
+              <div className="home-title-item">Team Player</div>
+              <div className="home-title-item">Creative</div>
+              <div className="home-title-item">Self-Motivated</div>
             </div>
+          
+          
           </div>
           <p className="home-description">
           I am a student at UWA to become a Full-Stack Web Developer.
           I am passionate about helping people to achieve their goals and I have found in web development the
           tools to continue doing that.
           </p>
-          
-        </div>
-        <div className='home-right'>
-          <div className='home-bg'></div>
-          <img src= { Me } alt='' className='me-img'/>
-        </div>
+          </div>
+       </div>
+         <div className='home-right'>
+          <div className='home-bg'>
+          <img src= { Photo } alt='' className='me-img'/>
+
+            <p>Mayra is Awesome</p>
+          </div>
+      
+
+        </div> 
       </div>
     </div>
           
-  )
-  }
+  )}

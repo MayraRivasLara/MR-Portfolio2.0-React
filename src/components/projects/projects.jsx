4eabb39aa433dc6f3fe881c2a019f5427f7fb22f
Mayra Rivas Lara <mayrulara@hotmail.com>
2022-06-10 00:15:02 +0800

    added projects screenshots and a file to contain the data of the projects

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import '../projects/projects.css';
-
+import { projectList } from "../../components/projects/projectsList";
+import { VscGithubAlt } from 'react-icons/vsc';
 
 
 
 export default function Projects({img, link, title}) {
+ 
   return (
-    <div className='project-card'>
-      <div className="p-browser">
-        <div className="p-circle"></div>
-        <div className="p-circle"></div>
-        <div className="p-circle"></div>
-      </div>
 
-      <a href={link} target="_blank" rel='noreferrer'>
-        <img src={img} alt="" className="project-image" />
-      </a>
+    <div className='container justify-content d-flex' id='project-card'>
+      <div className="row">
+        {projectList.map((data) => {
+          return(
+            <div className="card-wrapper col-12 col-md-6 col-lg-4 col-xl-3">
+              <div className='card'>
+                <img src={data.src} variant="top" className="card-src" alt="card-images" />
+                <div className="card-title"><h5>{data.title}</h5></div>
+                <div className="card-links">
+                <a href={data.deployedLink}> Deployed </a>|
+                <a href={data.githubLink}> Github <VscGithubAlt size="1.3rem" color="#00090d" /></a>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+        
+      </div>
 
     </div>
   )
+ 
 }
---
+++ src/components/projects/projectsList.js
@@ -0,0 +1,15 @@
+const projectList = [
+    {
+        title: "Comet Watch",
+        src: "https://i.ibb.co/6bFbLkq/comet-watch.png",
+        deployedLink: "https://wratten.github.io/Comet-Watch/",
+        githubLink: "https://github.com/LizMackle/Comet-Watch"
+    },
+    {
+        title: "Doggy-Day-Care",
+        src: "https://i.ibb.co/Jr3BKJC/Doggy-Day-Care.png",
+        deployedLink: "https://doggy-day-care-gula.herokuapp.com/",
+        githubLink: "https://github.com/opticsl8b/Doggy-Day-Care"
+    }
+]
+export { projectList };

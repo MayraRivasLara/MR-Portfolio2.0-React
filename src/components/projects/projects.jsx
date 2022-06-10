32409fa66f9ef151bd2a413d6f5ee707ebf194b1
Mayra Rivas Lara <mayrulara@hotmail.com>
2022-06-10 15:07:02 +0800

    projects page completed

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,34 +1,38 @@
 import React from 'react';
 import '../projects/projects.css';
 import { projectList } from "../../components/projects/projectsList";
-import { VscGithubAlt } from 'react-icons/vsc';
+import { VscGithubInverted } from 'react-icons/vsc';
 
 
 
 export default function Projects({img, link, title}) {
  
   return (
-
+    <div className="projects-page">
+    <><h1>Projects</h1>
     <div className='container justify-content d-flex' id='project-card'>
       <div className="row">
         {projectList.map((data) => {
-          return(
+          return (
+
             <div className="card-wrapper col-12 col-md-6 col-lg-4 col-xl-3">
               <div className='card'>
-                <img src={data.src} variant="top" className="card-src" alt="card-images" />
                 <div className="card-title"><h5>{data.title}</h5></div>
                 <div className="card-links">
-                <a href={data.deployedLink}> Deployed </a>|
-                <a href={data.githubLink}> Github <VscGithubAlt size="1.3rem" color="#00090d" /></a>
+                  <a href={data.deployedLink}> Website </a>   ||
+                  <a href={data.githubLink}>  <VscGithubInverted size="1rem" color="#00090d" /></a>
                 </div>
+                <img src={data.src} variant="top" className="card-src" alt="card-images" />
               </div>
             </div>
-          )
-        })}
-        
-      </div>
 
-    </div>
+            
+          );
+        })}
+
+      </div>
+    </div> </>
+ </div>
   )
  
 }
--- src/components/projects/projectsList.js
+++ src/components/projects/projectsList.js
@@ -10,6 +10,66 @@ const projectList = [
         src: "https://i.ibb.co/Jr3BKJC/Doggy-Day-Care.png",
         deployedLink: "https://doggy-day-care-gula.herokuapp.com/",
         githubLink: "https://github.com/opticsl8b/Doggy-Day-Care"
-    }
+    },
+    {
+        title: "WanderView",
+        src: "https://i.ibb.co/WVWfh70/Wander-View.png",
+        deployedLink: "https://wanderview.herokuapp.com/",
+        githubLink: "https://github.com/LizMackle/WanderView"
+    },
+    {
+        title: "My-Notes-Application",
+        src: "https://i.ibb.co/vL3zhXb/My-Notes-Aplication.png",
+        deployedLink: "https://frozen-eyrie-42428.herokuapp.com/",
+        githubLink: "https://github.com/MayraRivasLara/My-Notes-Application"
+    },
+    {
+        title: "MyTextEditor",
+        src: "https://i.ibb.co/FwB0Vd6/My-Text-Editor.png",
+        deployedLink: "https://mytexteditor-app.herokuapp.com/",
+        githubLink: "https://github.com/MayraRivasLara/MyTextEditor"
+    },
+    {
+        title: "MR - ECommerce",
+        src: "https://i.ibb.co/djqG07N/MR-ECommerce.png",
+        githubLink: "https://github.com/MayraRivasLara/MR-ECommerce"
+    },
+    {
+        title: "Today's Weather",
+        src: "https://i.ibb.co/7z5Ym8R/Todays-Weather.png",
+        deployedLink: "https://mayrarivaslara.github.io/Today-sWeather/",
+        githubLink: "https://github.com/MayraRivasLara/Today-sWeather"
+    },
+    {
+        title: "Social Network API",
+        src: "https://i.ibb.co/T0sHb8D/Social-Network-API.png",
+        githubLink: ""
+    },
+    {
+        title: "Work Day Organizer",
+        src: "https://i.ibb.co/2NH90Cc/Work-Day-Organizer.png",
+        deployedLink: "https://mayrarivaslara.github.io/WorkDayOrganizer/",
+        githubLink: "https://github.com/MayraRivasLara/WorkDayOrganizer"
+    },
+    {
+        title: "Secure Password Generator",
+        src: "https://i.ibb.co/GkM1006/Secure-Password-Generator.png",
+        deployedLink: "https://mayrarivaslara.github.io/Secure-Password-Generator/",
+        githubLink: "https://github.com/MayraRivasLara/Secure-Password-Generator"
+    },
+    {
+        title: "JS Quiz",
+        src: "https://i.ibb.co/P9YSY8K/JSQuiz.png",
+        deployedLink: "https://mayrarivaslara.github.io/JS-Quiz/",
+        githubLink: "https://github.com/MayraRivasLara/JS-Quiz"
+    },
+    {
+        title: "HoriseonMINIProject",
+        src: "https://i.ibb.co/0KW8DXG/Horiseon.png",
+        deployedLink: "https://mayrarivaslara.github.io/HoriseonMINIProject/",
+        githubLink: "https://github.com/MayraRivasLara/HoriseonMINIProject"
+    },
+
+
 ]
 export { projectList };
